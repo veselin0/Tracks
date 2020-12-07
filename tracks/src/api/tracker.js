703d8c168f34eeffd7c92641ba6f5df264d7357f
Veselin <vesselin60@gmail.com>
2020-12-07 10:25:56 +0200

    260. Automatic Authentication

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'http://cce69db322bb.ngrok.io',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err),
+);
+
+export default instance;
